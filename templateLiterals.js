069fbcf8ba49fa4ecce99353e5181dbdab504cd6
Rinku Maurya <126308215+rinkumaurya23@users.noreply.github.com>
2024-03-08 16:27:26 +0000

    templateLiterals

---
+++ templateLiterals.js
@@ -0,0 +1,26 @@
+// ?  concation  kind of template literal 
+
+
+// let quote = "either you run the code or code runs you";
+// let  by = "Rinku Maurya";
+// let user = "shashi"
+// //  console.warn(quote + by)  either you run the code or code runs youRinku Maurya
+// console.log(`${quote} ${by} ${user}`) //either you run the code or code runs you Rinku Maurya
+
+
+// function fullName(){
+//     return "Rinku Maurya";
+// }
+
+// console.log(fullName()) // Rinku Maurya
+// const a = 5;
+// const b = 10;
+// console.log(`Fifteen is ${a + b} and
+// not ${2 * a + b}.`);
+// // "Fifteen is 15 and
+// // not 20."
+
+
+const classes = `header ${
+    isLargeScreen() ? "" : `icon-${item.isCollapsed ? "expander" : "collapser"}`
+  }`;
