31fd2252d6f38627b48600592205617b6e78dc67
Rinku Maurya <126308215+rinkumaurya23@users.noreply.github.com>
2024-03-08 09:50:33 +0000

    major topic

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("Ecma Script")
+
+//?  Major Topic 
+/* 
+1.Let, const 
+2.Arrow Function
+3.Class
+4.Promise
+5.Rest Parameters
+6.Array Prototype
+7.** Operator
+8.Shared Mamory
+9.Async Await
+10. Symbol Protype
+
+
+*/
